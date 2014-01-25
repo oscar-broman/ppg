4bb081e336b6e3780177915d8ec08f1f361a0e63
Oscar Broman <oscar.broman@gmail.com>
2014-01-25 23:45:45 +0100

    add samp-introspect

--- public/js/ppg-run.js
+++ public/js/ppg-run.js
@@ -3,9 +3,15 @@
 window.PPG = (function (self) {
   var socket = io.connect(location.protocol + '//' + location.hostname);
   var runId = 0;
+  var running = false;
   
   $('#run-code').on('click', function(e) {
     self.clearOutput();
+    
+    running = true;
+    
+    $('#run-code').attr('disabled', true);
+    $('#stop-run').removeAttr('disabled');
     
     socket.emit('run-code', {
       code: self.getEditorValue(),
@@ -16,12 +22,51 @@ window.PPG = (function (self) {
     });
   });
   
+  $('#stop-run').on('click', function(e) {
+    socket.emit('stop-run', {
+      runId: runId
+    });
+  });
+  
+  $('#output-prompt').on('keyup', function(e) {
+    var $this = $(this);
+    
+    if (e.which === 13) {
+      var val = $.trim($this.val());
+      
+      socket.emit('run-line', {
+        code: val,
+        runId: runId
+      });
+      
+      $this.val('');
+    }
+  });
+  
+  $(window).on('keydown', function(e) {
+    if (e.shiftKey && (e.metaKey || e.ctrlKey)) {
+      if (e.which === 82) { // R
+        $('#run-code').trigger('click');
+        
+        return false;
+      } else if (e.which === 83) { // S
+        $('#stop-run').trigger('click');
+        
+        return false;
+      } else if (e.which === 69) { // E
+        $('#output-prompt').focus();
+      }
+    }
+  });
+  
   socket.on('connect', function() {
-    $('#run-btn button').removeAttr('disabled');
+    $('#run-code').removeAttr('disabled');
   });
   
   socket.on('disconnect', function() {
-    $('#run-btn button').attr('disabled', true);
+    $('#run-code, #stop-run').attr('disabled', true);
+    
+    running = false;
   });
   
   socket.on('compiler-output', function(output) {
@@ -38,6 +83,13 @@ window.PPG = (function (self) {
     }
     
     self.appendOutput(output.data);
+  });
+  
+  socket.on('server-stopped', function() {
+    running = false;
+    
+    $('#stop-run').attr('disabled', true);
+    $('#run-code').removeAttr('disabled');
   });
   
   return self;
