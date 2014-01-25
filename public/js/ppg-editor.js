93c0d1124d37b5e40052643c6b8b27f0255c7e53
Oscar Broman <oscar.broman@gmail.com>
2014-01-26 00:06:16 +0100

    fix jshint warnings

--- public/js/ppg-editor.js
+++ public/js/ppg-editor.js
@@ -3,39 +3,39 @@
 window.PPG = (function (self) {
   var Range = ace.require('./range').Range;
 	var editor = ace.edit('editor-field');
-  
+
   editor.setTheme('ace/theme/textmate');
   editor.setBehavioursEnabled(false);
-  
+
   if (window.localStorage && localStorage.editorText) {
     editor.setValue(localStorage.editorText || '');
     editor.clearSelection();
   }
-  
+
   self.startEditorSession = function(text) {
     var session = ace.createEditSession(text || '', mode);
-    
+
     session.setMode('ace/mode/c_cpp');
     session.setUseSoftTabs(false);
     session.setFoldStyle('markbeginend');
-    
+
     editor.setSession(session);
     editor.clearSelection();
   };
-  
+
   self.getEditorValue = function() {
     return editor.getValue();
   };
-  
+
   self.setEditorFocus = function() {
     editor.focus();
   };
-  
+
 	$(window).on({
 	  unload: function(e) {
       localStorage.setItem('editorText', self.getEditorValue());
 	  }
 	});
-	
+
   return self;
 }(window.PPG || {}));
