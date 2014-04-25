bea2407128ed62ac7bbc99eedfacc5cfd79ba9e5
Oscar Broman <oscar.broman@gmail.com>
2014-04-25 10:40:56 +0200

    add GitHub OAuth sign-in and load gists

--- public/js/ppg-gist.js
+++ public/js/ppg-gist.js
@@ -51,5 +51,80 @@ window.PPG = (function (self) {
     }
   };
 
+  var GHAccessToken = null;
+
+  OAuth.initialize('829L698cmvBJTonf9Tsje6iUqCM', {
+    cache: true
+  });
+
+  $('#github-signin').on({
+    click: function() {
+      OAuth.popup('github', function (err, result) {
+        if (err) {
+          alert('GitHub authentication failed.');
+
+          OAuth.clearCache();
+
+          return;
+        }
+
+        $('.github-signin').hide();
+
+        GHAccessToken = result.access_token;
+        window.GHAccessToken=GHAccessToken;
+
+        localStorage.ppgGHAccessToken = GHAccessToken;
+
+        loadGists();
+      });
+    }
+  });
+
+  if (localStorage.ppgGHAccessToken) {
+    GHAccessToken = localStorage.ppgGHAccessToken;
+
+    $('.github-signin').hide();
+
+    loadGists();
+  }
+
+  function loadGists() {
+    $('.github-gists')
+      .empty()
+      .text('Loading gists..');
+
+    $.ajax({
+      url: 'https://api.github.com/gists',
+      dataType: 'json',
+      beforeSend: function(xhr) {
+        xhr.setRequestHeader('Authorization', 'Bearer ' + GHAccessToken);
+      }
+    }).done(function(data) {
+      $('.github-gists').text('');
+
+      data.forEach(function(gist, idx) {
+        var files = Object.keys(gist.files);
+
+        if (files.length !== 1) {
+          return;
+        }
+
+        if (!gist.files[files[0]].filename.match(/\.(inc|pwn)$/i)) {
+          return;
+        }
+
+        $('.github-gists').append(
+          $('<a class="list-group-item" href="#"/>')
+            .text(gist.description || gist.files[files[0]].filename)
+            .data('gist-idx', idx)
+        );
+      });
+
+      console.log(data)
+    }).fail(function(err) {
+      console.log(err);
+    });
+  }
+
   return self;
 }(window.PPG || {}));
